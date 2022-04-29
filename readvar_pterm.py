97e8ba8d0cc31b506a309fba79ddbf179b0e4c30
milankkk <keymolen9@outlook.com>
2022-04-29 23:00:57 +0200

    add

--- readvar_pterm.py
+++ readvar_pterm.py
@@ -1,4 +1,4 @@
-#! /usr/bin python3
+# /usr/bin python3
 import random
 import time
 
@@ -6,9 +6,9 @@ with open('out.txt','a') as f:
     f.truncate(0)
     f.seek(0)
 for i in range(10):
-    time.sleep(3)
+    time.sleep(1)
     random.seed()
     n = random.random()
-    #with open('out.txt', 'a') as f:
-    #    f.write(str(n) + '\n')
+    with open('out.txt', 'a') as f:
+        f.write("n = " + str(n) + '\n')
     print(n)
