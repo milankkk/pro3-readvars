e7af357e6ebbe1c828d98d56788ef3a6db86b639
milankkk <38917942+milankkk@users.noreply.github.com>
2022-04-29 15:39:19 +0200

    10 random var values + write to file

--- readvar.py
+++ readvar.py
@@ -1,6 +1,11 @@
+#! /usr/bin python3
 import random
 
-
-n = random.random()
-print(n)
-
+with open('out.txt','a') as f:
+    f.truncate(0)
+    f.seek(0)
+for i in range(10):
+    random.seed()
+    n = random.random()
+    with open('out.txt', 'a') as f:
+        f.write(str(n) + '\n')
